d4df6ffd5f5e964c6aa88e352a5679f4b519dea8
ehre0004 <ehre0004@algonquinlive.com>
2024-11-12 14:04:15 -0500

    commented code

--- EmpowerAbilityLab.js
+++ EmpowerAbilityLab.js
@@ -6,39 +6,58 @@ function knowledgeRunner(){
 knowledgeRunner()
 
 
-// Rae Ehret's workspace
-// navigation
-var dropdowns = document.getElementsByClassName("dropdown");
 
+/* ************************************** Rae Ehret's Workspace *************************************** */
+/* ******************************************** NAVIGATION ******************************************** */
+// prevent default behaviour (navigating to "#" or whatever href value is set)
+var navlinks = document.getElementsByClassName("nav-item");
+for (let i=0; i<navlinks.length; i++) {
+    navlinks.item(i).addEventListener("click", preventDefaultBehaviour, false);
+}
+
+/**
+ * Prevent default behaviour, i.e. navigating by href value
+ * @param {Event} e 
+ */
+function preventDefaultBehaviour(e) {
+    e.preventDefault();
+}
+
+// navigation dropdown behaviour
+// this code navigates the DOM and sets event listeners to the toggles
+// [li] should be class "dropdown", toggling links [a] "dropdown-toggle", and menus of links [ul] "dropdown-menu"
+var dropdowns = document.getElementsByClassName("dropdown");
 for (let i=0; i<dropdowns.length; i++) {
     let toggle, param;
-    let toggles = dropdowns.item(i).children;
-    for (let y=0; y<toggles.length; y++) {
-        if (toggles.item(y).classList.contains("dropdown-toggle")) {
-            toggle = toggles.item(y);
-            param = toggle;
+    let nodes = dropdowns.item(i).children;
+    for (let y=0; y<nodes.length; y++) {
+        // check for the toggling node amongst the child nodes, and set our toggling node
+        if (nodes.item(y).classList.contains("dropdown-toggle")) {
+            toggle = nodes.item(y);
+            param = toggle; // We don't really need to specify a separate variable for toggle, I just find it more readable this way.
         }
     }
     // bind event listener to toggle
-    toggle.tparam = param;
+    toggle.tparam = param; // Alternatively: toggle.tparam = toggle // (passing itself)
     toggle.addEventListener("click", toggleDropdownMenu, false);
 }
 
 /**
- * 
+ * Toggles the "show" class on a toggling link's related menu (marked as class "dropdown-menu")
  * @param {Event} e
  */
 function toggleDropdownMenu(e) {
-    e.preventDefault;
+    // another function does this, but just in case:
+    e.preventDefault();
+    // traverse the dom to find the dropdown-menu node on the same level as the toggle
     let nodes = e.currentTarget.tparam.parentElement.children;
-    console.log("nodes: "+nodes);
     let menu;
     for (let element of nodes) {
         if (element.classList.contains("dropdown-menu")) {
             menu = element;
         }
     }
-    // toggle
+    // toggle behaviour
     if (menu.classList.contains("show")) {
         menu.classList.remove("show");
     } else {
